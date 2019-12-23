2570e39751f5065b74c60bdbda309914014afa53
jcmartinez <jcmartinezcu@gmail.com>
2019-12-23 19:58:23 +0100

    video 36

--- src/main/java/com/bolsadeideas/springboot/di/app/models/service/MiServicio.java
+++ src/main/java/com/bolsadeideas/springboot/di/app/models/service/MiServicio.java
@@ -1,13 +1,15 @@
 package com.bolsadeideas.springboot.di.app.models.service;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+@Primary
 @Component("miServicioSimple")
 public class MiServicio implements IServicio {
 	
 	@Override
 	public String operacion() {
-		return "Ejecuntando algún proceso importante....";
+		return "Ejecuntando algún proceso importante simple....";
 	}
 
 }
